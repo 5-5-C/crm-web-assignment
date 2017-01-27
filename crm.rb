ff2522c0520473dd5c3c7e77ebfb85d0ad725cb5
5-5-C <jmarshall.brock@gmail.com>
2017-01-27 17:03:27 -0500

    added edit feature

--- crm.rb
+++ crm.rb
@@ -27,7 +27,20 @@ post '/contacts' do
   redirect to('/contacts')
 end
 
-get '/contacts/1' do
-  @contact = Contact.find(1)
-  erb :show_contact
+get '/contacts/:id' do
+  @contact = Contact.find(params[:id].to_i)
+  if @contact
+    erb :show_contact
+  else
+    raise Sinatra::NotFound
+end
+end
+
+get '/contacts/:id/edit' do
+  @contact = Contact.find(params[:id].to_i)
+  if @contact
+    erb :edit_contact
+  else
+    raise Sinatra::NotFound
+  end
 end
